2f5fc6b8cb303aa8a7d7c225ec337b15c87b80c1
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-03-31 12:07:55 +0530

    Tweet button added

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -10,6 +10,7 @@ import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import SidebarOption from './SidebarOption'
+import { Button } from '@material-ui/core'
 
 
 function Sidebar() {
@@ -24,6 +25,7 @@ function Sidebar() {
             <SidebarOption Icon={ListAltIcon} text="Lists" />
             <SidebarOption Icon={PermIdentityIcon} text="Profile" />
             <SidebarOption Icon={MoreHorizIcon} text="More" />
+            <Button variant='outlined' className='sidebar__tweet' fullWidth>Tweet</Button>
         </div>
     )
 }
