4f3a2480964f404c5800bacd44fc3533b1c00985
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-03-31 18:36:47 +0530

    Firebase support added

--- src/Components/Feeds/Feed.js
+++ src/Components/Feeds/Feed.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import './Feed.css'
 import Post from './Post/Post'
 import TweetBox from './TweetBox'
+import db from '../../firebase'
 
 function Feed() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        ))
+    }, [])
     return (
         <div className='feed'>
             <div className='feed__header'>
@@ -11,11 +19,17 @@ function Feed() {
             </div>
 
             <TweetBox/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+
+            {posts.map(post => (
+                <Post
+                displayName = {post.displayName}
+                username={post.username}
+                verified={post.verified}
+                text={post.text}
+                avatar={post.avatar}
+                image={post.image} />
+            ))}
+            
         </div>
     )
 }
